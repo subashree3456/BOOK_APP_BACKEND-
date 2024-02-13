1bcfce99a9abaeea48c25b564bb5d21992d603c8
subashree3456 <107339951+subashree3456@users.noreply.github.com>
2024-02-13 10:10:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+
+const databaseConnection = async() => {
+ 
+  try {
+    const connection = await mongoose.connect("mongodb+srv://admin:X3l66mbVjfnAW1Nb@cluster0.omkc6tc.mongodb.net/Book-StoreretryWrites=true&w=majority")
+    console.log("Connected to the mongoDB");
+   
+
+} catch (error) {
+    console.log(error);
+}
+ 
+}
+
+databaseConnection();
+
+
+app.listen(4000, () =>{
+console.log("App is listening on this...")
+})
+
+
+
+
+
+
+
+// admin
+
+// X3l66mbVjfnAW1Nb
+
+
+// new pwd --> F94P9SFL1sAMAcDJ
